bcb3124d21f60be631b581a1cdbcf63b2b227725
Kimia Abtahi <kimia.abtahi@bibsent.no>
2024-10-03 17:08:13 +0200

    add a footer that shows the date of the page rendering

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import "./styles.css";
 
 function RootLayout({ children }) {
+  const date = new Date().toLocaleString();
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>Page rendered on {date}</footer>
       </body>
     </html>
   );
